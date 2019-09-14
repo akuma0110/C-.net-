1150f166d52aa7a05e698ab13ffd108e14718879
akuma0110 <52898737+akuma0110@users.noreply.github.com>
2019-09-14 13:20:00 +0700

    shap

---
+++ Shap/Shap/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            rectangular r = new rectangular();
+            r.area(2, 5);
+            Console.WriteLine(r);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Shap/Shap/shap1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shap
+{
+    abstract class shap1
+    {
+        public abstract double perimeter();
+        public abstract double area(double weigth, double length);
+    }
+    
+    class rectangular : shap1
+    {
+        double weigth;
+        double length;
+
+        public rectangular()
+        {
+        }
+
+        public rectangular(double weigth, double length)
+        {
+            this.weigth = weigth;
+            this.length = length;
+        }
+
+        public double Weigth { get => weigth; set => weigth = value; }
+        public double Length { get => length; set => length = value; }
+
+        public override double area(double weigth, double length)
+        {
+            return weigth * length;
+        }
+
+        public override double perimeter()
+        {
+            return (weigth + length) * 2;
+        }
+    }
+
+}
