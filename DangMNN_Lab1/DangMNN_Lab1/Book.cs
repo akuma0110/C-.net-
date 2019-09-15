5bac50cd42109983ba801e58222bca77ae8e4b76
akuma0110 <52898737+akuma0110@users.noreply.github.com>
2019-09-15 20:01:13 +0700

    lab1

---
+++ DangMNN_Lab1/DangMNN_Lab1/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DangMNN_Lab1
+{
+    class Book
+    {
+        public void testsavefile()
+        {
+           
+        }
+    }
+}
---
+++ DangMNN_Lab1/DangMNN_Lab1/BookDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DangMNN_Lab1
+{
+    class BookDTO
+    {
+        private String id, name, publisher, price;
+
+        public BookDTO()
+        {
+        }
+
+        public BookDTO(string id, string name, string , string publisher, string price)
+        {
+            this.id = id;
+            this.name = name;
+            this. = ;
+            this.publisher = publisher;
+            this.price = price;
+        }
+
+        public string Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Publisher { get => publisher; set => publisher = value; }
+        public string Price { get => price; set => price = value; }
+    }
+}
