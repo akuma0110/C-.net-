d0dbccc9e7222d2061dfc8cbbd26facee9ab3039
akuma0110 <52898737+akuma0110@users.noreply.github.com>
2019-09-19 09:21:47 +0700

    mathlib

---
+++ MathLib/MathLib/MyMath.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLib
+{
+    public class MyMath
+    {
+        //phép cộng
+        public int add(int a , int b)
+        {
+            return a + b;
+        }
+
+        //phép trừ
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        //phép nhân
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        //phép chia
+
+        public int div(int a, int b)
+        {
+            return a / b;
+        }
+
+    }
+}
---
+++ testMathlib/testMathlib/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathLib;
+
+namespace testMathlib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath math = new MyMath();
+            Console.WriteLine(math.add(5, 6));
+            Console.ReadLine();
+        }
+    }
+}
