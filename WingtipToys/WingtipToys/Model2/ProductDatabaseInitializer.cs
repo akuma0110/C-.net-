00632e217f6bfd9e3efdf34db524d8ecad378521
akuma0110 <52898737+akuma0110@users.noreply.github.com>
2019-10-03 10:15:53 +0700

    webform

---
+++ WingtipToys/WingtipToys/Model2/ProductDatabaseInitializer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WingtipToys.Model2
+{
+    public class ProductDatabaseInitializer : DropCreateDatabaseAlways<ProductContext>
+    {
+
+        protected override void Seed(ProductContext context)
+        {
+            GetCategories().ForEach(c => context.Categories.Add(c));
+            GetProducts().ForEach(p => context.Products.Add(p));
+        }        
+        private static List<Category> GetCategories()
+        {
+            var category = new List<Category>
+            {
+                new Category
+                {
+                    CategoryID = 1,
+                    CategoryName = "Cars"
+                },
+                new Category
+                {
+                    CategoryID = 2,
+                    CategoryName = "Motobike"
+                }
+            };
+            return category;
+        }
+
+        private static List<Product> GetProducts()
+        {
+            var product = new List<Product>
+            {
+                new Product
+                {
+                    ProductID = 1,
+                    ProductName = "Convertible Car",
+                    Description =  "This convertible car is fast! The engine is powered by a neutrino based battery (not included)." +
+ "Power it up and let it go!",
+                    ImagePath="carconvert.png",
+                    UnitPrice = 22.50,
+                    CategoryID = 1                },
+                new Product
+                {
+                    ProductID = 2,
+                    ProductName = "Old-Time Car",
+                    Description =  "This convertible car is fast! The engine is powered by a neutrino based battery (not included)." +
+ "Power it up and let it go!",
+                    ImagePath="carearly.png",
+                    UnitPrice = 15.95,
+                    CategoryID = 2
+                }
+            };
+            return product;
+        }
+    }
+}
